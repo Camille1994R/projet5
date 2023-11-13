276063cac4efa7de4a3044265c44e9ea3e1d8273
Camille Rousset <yuchanx3@gmail.com>
2023-11-13 11:33:54 +0100

    carrousel sans point ou tagline

--- assets/script.js
+++ assets/script.js
@@ -17,20 +17,42 @@ const slides = [
 	}
 ]
 
+const slidesImages = ["slide1.jpg", "slide2.jpg", "slide3.jpg", "slide4.jpg" ]
+
 let currentSlide = 0;
 
 let arrow_right = document.querySelector(".arrow_right")
 arrow_right.addEventListener("click",function () {
 	console.log("vous avez cliqué sur le bouton")
-	alert("Salut à droite!")
+	//alert("Salut à droite!")
 });
 let arrow_left = document.querySelector(".arrow_left")
 arrow_left.addEventListener("click",function () {
 	console.log("vous avez cliqué sur le bouton")
-	alert("Salut (à gauche)!")
+	//alert("Salut (à gauche)!")
 });
 
 const NombrePoints = slides.length
 console.log(NombrePoints)
 //résultat : 4
+
+//let bannerChange = document.getElementById('mainBanner')
+//bannerChange.setAttribute('src', './assets/images/slideshow/slide2.jpg')
+
+
+function changeSlide (sens) {
+	currentSlide = currentSlide + sens;
+	if (currentSlide > 3)
+		currentSlide = 0
+	if (currentSlide < 0)
+		currentSlide = 3
+	document.getElementById("mainBanner").src= "./assets/images/slideshow/" + slidesImages[currentSlide];
+
+}
+
+let contenuP1 = "Tirages haute définition grand format <span>pour vos bureaux et events</span>";
+let contenuP2 = "Grand choix de couleurs <span>de CMJN aux pantones</span>";
+let contenuP3 = "Autocollants <span>avec découpe laser sur mesure</span>"
+
+let p = document.querySelector(".tagLine")
 
